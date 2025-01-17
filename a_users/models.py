0d3bd0045da36a4f6db04541acb7a8da293476f9
Victor Cantun <victorcantun@icloud.com>
2025-01-17 10:45:35 -0600

    Update modelo profile

--- a_users/models.py
+++ a_users/models.py
@@ -6,14 +6,14 @@ from core.models import dependence
 class Profile(models.Model):
     user_role=[
         (1,'citizen'),
-        (2,'employee')
+        (2,'employe')
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="avatars/", null=True, blank=True)
     displayname = models.CharField(max_length=20, null=True, blank=True, verbose_name="Nombre Completo")
     info = models.TextField(null=True, blank=True, verbose_name="Információn complementaria")
     role = models.IntegerField(verbose_name="Rol",choices=user_role,default=1)
-    dependence = models.OneToOneField(dependence, on_delete=models.CASCADE,null=True, blank=True)
+    dependence = models.ForeignKey(dependence, on_delete=models.CASCADE,null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
